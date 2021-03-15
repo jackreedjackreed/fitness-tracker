bac35c07d66f4976b7bf582d36dd0232e7774366
J-Boogie <jackr353@gmail.com>
2021-03-15 14:36:00 -0500

    add api routes from same activity for reference

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,41 @@ const express = require("express");
 const router = express.Router();
 
 
+// const router = require("express").Router();
+// const Transaction = require("../models/transaction.js");
+
+// router.post("/api/transaction", ({ body }, res) => {
+//   Transaction.create(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.post("/api/transaction/bulk", ({ body }, res) => {
+//   Transaction.insertMany(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.get("/api/transaction", (req, res) => {
+//   Transaction.find({})
+//     .sort({ date: -1 })
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+module.exports = router;
 
 
 module.exports = router
