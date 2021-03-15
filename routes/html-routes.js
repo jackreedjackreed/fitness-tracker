adfa11e0b3abcb069f6b368498ef534afa7da43d
J-Boogie <jackr353@gmail.com>
2021-03-15 13:05:06 -0500

    add html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,31 @@
 const path = require('path');
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
+// index
+router.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+// index 
+router.get("/index", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+// exercise
+router.get("/exercise", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+// stats
+router.get("/stats", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+
+// all other routes --> index
+router.get("*", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
 
 
 module.exports = router;
