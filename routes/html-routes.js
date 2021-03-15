8baa60c4866da300221ca6c66550d218609a2834
J-Boogie <jackr353@gmail.com>
2021-03-15 15:37:04 -0500

    data seems to not be entering the db or i'm not retrieving it correctly, debugging...

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,10 +22,10 @@ router.get("/stats", function(req,res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
-// all other routes --> index
-router.get("*", function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-});
+// // all other routes --> index
+// router.get("*", function(req,res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
 
 
 module.exports = router;
